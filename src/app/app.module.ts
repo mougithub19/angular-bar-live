513c7061a412593147bf8c93f1138ae985303a40
mougithub19 <moubusiness18@gmail.com>
2020-03-22 13:34:34 +0530

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ChartsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
